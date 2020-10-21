895ff0752dbf838d6320772422b6560d26db4d66
fatteme <fatt3me@gmail.com>
2020-10-21 11:41:06 +0330

    tests added but it doesnt work correctly

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include lex.yy.h
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std ;
+
 extern FILE *yyin,*yyout;
 
 int main(int argc, char* argv[]){
@@ -16,11 +18,10 @@ int main(int argc, char* argv[]){
     string input_file_path = argv[2];
     string output_file_path = argv[4];
 
-    ofstream output_file(output_file_path) ;
-    ifstream input_file
-    fprintf(yyout);
-    yyin = fopen(input_file_path, "r");
-    yyout = fopen(output_file_path, "w");
+    ofstream output_file(output_file_path);
+    ifstream input_file;
+    fprintf(yyout, "s");
+    yyin = fopen(input_file_path.c_str(), "r");
+    yyout = fopen(output_file_path.c_str(), "w");
     
-
 }
