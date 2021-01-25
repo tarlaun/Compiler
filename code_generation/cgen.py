b403b6c4f1882ddb7379dbba717dbc5f51dc84a1
fatteme <fatt3me@gmail.com>
2021-01-25 21:14:37 +0330

    initial structure of cgen

---
+++ code_generation/cgen.py
@@ -0,0 +1,26 @@
+def if_cgen():
+    return None
+
+
+def for_cgen():
+    return None
+
+
+def while_cgen():
+    return None
+
+
+def function_cgen():
+    return None
+
+
+def statement_cgen():
+    return None
+
+
+def class_cgen():
+    return None
+
+
+if __name__ == '__main__':
+    print('this is cgen.')
